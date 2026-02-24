400307bccee89d08c5cfc5f33fd2d3ea6285e8b5
mrlexcoder <mrlexcoder@gmail.com>
2026-02-24 21:42:04 +0530

    woks on all case like case studies pages

---
+++ JS/Day5-6-2/Allfun.js
@@ -0,0 +1,2 @@
+// what is single trader and multiple thread  with example
+// and aslo process multi processes 
---
+++ JS/day6-6-64/js_closer.js
@@ -0,0 +1,3 @@
+console.log("This is a message from the main thread.");
+
+// what is js and is it single threaded or multi threaded
