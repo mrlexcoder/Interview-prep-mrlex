3da49ab5ca0b9c90b3ee145ad83dc6db9d36d12a
mrlexcoder <mrlexcoder@gmail.com>
2026-03-02 00:14:45 +0530

    working on array numbers

--- Prep-Leetcode/First.js
+++ Prep-Leetcode/First.js
@@ -1,6 +1,6 @@
 // add two arrays in js
 
-let a = [1,2,3];
+let a = [3,33,6,3,1,2,3];
 let b = [2,3,4,5];
 
 let result = [...a,...b];
@@ -8,3 +8,12 @@ let result = [...a,...b];
 let resultwithnorepeat=[...new Set([...a,...b])];
 
 console.log(result);
+
+
+
+// want to get only repeated numbers from arrays
+let repeated = result.filter((value, index, arr) => 
+  arr.indexOf(value) !== index
+);
+
+console.log([...new Set(repeated)]);
