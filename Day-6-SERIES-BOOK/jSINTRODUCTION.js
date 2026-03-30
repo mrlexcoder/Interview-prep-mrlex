dcd6c08039247f6f5c746fc7c347fe40ebb9f425
mrlexcoder <mrlexcoder@gmail.com>
2026-03-30 17:27:26 +0530

    push

---
+++ Day-6-SERIES-BOOK/jSINTRODUCTION.js
@@ -0,0 +1,22 @@
+console.log("This is a message from the main thread.");
+
+// what is js and is it single threaded or multi threaded
+// JavaScript is a high-level, interpreted programming language that is commonly 
+// used for web development. It is primarily used to create interactive and dynamic
+//  content on websites. JavaScript is single-threaded, meaning it executes code in 
+// a single sequence, one operation at a time. However, it can handle asynchronous 
+// operations using callbacks, promises, and async/await, allowing it to perform tasks
+//  without blocking the main thread.
+
+// what is web worker in JavaScript?
+// A Web Worker in JavaScript is a background thread that allows you to run scripts
+//  in the background without blocking the main thread. It enables you to perform tasks 
+// that are computationally intensive or time-consuming without affecting the
+//  responsiveness of the user interface. Web Workers communicate with the main
+//  thread using messages, allowing you to offload heavy processing tasks and
+//  keep the UI responsive.
+// how can we use as multi thread waht is thread with ex in js
+// In JavaScript, you can achieve multi-threading using Web Workers. A thread 
+// is a separate path of execution in a program. With Web Workers, you can create 
+// a new thread that runs concurrently with the main thread, allowing you to perform
+//  tasks without blocking the user interface.   
